9ef36cbd995489f425d92f7575b025a38516a21e
Taha Siraj <160783918+Tahasiraj1@users.noreply.github.com>
2024-10-21 19:08:28 +0500

    Update Landing-page.tsx

--- components/Landing-page.tsx
+++ components/Landing-page.tsx
@@ -15,7 +15,7 @@ const LandingPage = () => {
                 Welcome To Our Website
             </h1>
             <p className='font-normal text-[18px] lg:text-[32px] lg:w-[571px] h-[116px] lg:h-[156px] top-[318px] left-[68px] text-gray-500 mb-0 lg:mb-20'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&aposs standard
             </p>
             <button
             className='w-[190px] lg:w-[237px] h-[50px] lg:h-[66px] text-[18px] lg:text-[28px] bg-black text-white mt-0'
